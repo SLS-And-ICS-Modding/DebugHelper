3a6f235f58cc844fc133acdb01cf95f3eb5daf86
Zordon <65111609+Zordon1337@users.noreply.github.com>
2024-03-01 15:59:44 +0100

    DebugHelper Release

---
+++ DebugHelper/MainClass.cs
@@ -0,0 +1,41 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DebugHelper
+{
+    
+    public class MainClass:MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            
+        }
+       
+        public override void OnUpdate()
+        {
+            
+        }
+        
+        public override void OnGUI()
+        {
+            Color oldcolor = GUI.color;
+            GUI.color = Color.yellow;
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            GUILayout.Label(Camera.main.transform.position.ToString());
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 25f))
+            {
+                GameObject hitObject = hit.collider.gameObject;
+                GUILayout.Label("Looking at(by name): " + hitObject.name);
+                GUILayout.Label("Looking at(by tag): " + hitObject.tag);
+            }
+            GUI.color = oldcolor;
+        }
+
+    }
+}
--- DebugHelper/Utilities.cs
+++ DebugHelper/Utilities.cs
@@ -4,20 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-namespace ModTemplate
+namespace DebugHelper
 {
-    /*
-     * 
-     * basically class containing basic
-     * utilites, remember most of variables like IsInGame
-     * should work in most Cheesecake dev games but it may
-     * not work in some games
-     */
-
-    /*
-     * if you want to mod any other game than Internet Cafe Simulator
-     * edit Properties->AssemblyInfo.cs
-     */
+    
     public static class Utilities
     {
         public static bool IsInGame => GameObject.FindGameObjectsWithTag("Player").Length <= 0;
